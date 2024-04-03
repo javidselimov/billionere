437420df459f785aac6b9694a41b5d03604f7cb1
JavidZS <javid.salimov@aist.group>
2024-04-03 19:29:36 +0400

    added chnages

--- input/main.js
+++ input/main.js
@@ -2,7 +2,10 @@ const inputName = document.querySelector('#name')
 const inputAge = document.querySelector('#age')
 const pName = document.querySelector('.name')
 const pAge = document.querySelector('.age')
+const pGender = document.querySelector('.gender')
 const button = document.querySelector('button')
+const selectBox = document.querySelector('select')
+const subscribeCheckbox = document.querySelector('.subscribe');
 
 let dummyData = {}
 
@@ -29,7 +32,22 @@ inputName.addEventListener('change',(e)=>{
 button.addEventListener('click',()=>{
     pAge.innerText = dummyData.age
     pName.innerText = dummyData.name
+    pGender.innerText = dummyData.gender
 })
+
+selectBox.addEventListener("change",(e)=>{
+    console.log(e.target.value)
+    dummyData[e.target.name]=e.target.value
+})
+
+subscribeCheckbox.addEventListener('change', (event) => {
+  if (event.target.checked) {
+    alert(event.target.checked,'Siz abunə oldunuz');
+  } else {
+    alert('Siz abunə siyahısından çıxarılmısınız');
+  }
+});
+
 
 
 
